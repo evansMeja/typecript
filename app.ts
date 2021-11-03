39d97db6f81baa332d3da46bd40fe65760d3e44d
Evans Kipagat <evanslagat911@gmail.com>
2021-11-03 14:47:21 +0300

    UION TYPE

--- app.ts
+++ app.ts
@@ -1,16 +1,23 @@
-function combine(input1: number | string, input2: number | string){
+function combine(
+    input1: number | string, 
+    input2: number | string, 
+    resultConversion: 'as-text' | 'as-number'
+    ){
     let result;
-    if(typeof input1 === 'number' && typeof input2 === 'number'){
-        result = input1 + input2
+    if(typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number'){
+        result = +input1 + +input2
     }else{
         result = input1.toString() + input2.toString()
     }
     return result
 }
 
-const result1 = combine(1,3)
+const result1 = combine(1,3, 'as-number')
 
-const result2 = combine('Evans','Meja')
+const result2 = combine('39','56', 'as-number')
+
+const result3 = combine('Evans','Meja','as-text')
 
 console.log(result1)
 console.log(result2)
+console.log(result3)
