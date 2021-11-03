1e51eb4f8852ba9b0165d4ddc27ae70873bb5502
Evans Kipagat <evanslagat911@gmail.com>
2021-11-03 12:30:35 +0300

    TUPPLE

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-function add(n1, n2) {
-    return n1 + n2;
+var Person = {
+    name: 'Evans Meja',
+    age: 23,
+    hobbies: ['coding', 'cooking', 'music']
+};
+console.log(Person);
+var favActivities;
+favActivities = ['sports'];
+for (var _i = 0, _a = Person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toLocaleUpperCase());
 }
-var number1 = 5;
-var number2 = 2.8;
-var result = add(5, 2.8);
-console.log(result);
